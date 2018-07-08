fb4d8367c150227fb3d9b3bc87220f0fd4c0dd99
AlexCastillaG <alexcastilladj@gmail.com>
2018-07-08 12:50:36 +0200

    .

--- 360Robot.ino
+++ 360Robot.ino
@@ -1,4 +1,4 @@
-/* p
+/* 
  * Developed by Makers Gandía in colaboration with Immersive Interactive Lab.
  * Authors: Alejandro Marco Ibáñez, María Balagué, Leonardo Rodríguez, Alejandro Castilla García, Jair López Gutiérrez
 */
