99d4e89088ea7eb73d77b7e27869ea19ecc680a1
AlexCastillaG <alexcastilladj@gmail.com>
2018-07-08 12:59:38 +0200

    .

--- 360Robot.ino
+++ 360Robot.ino
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* p
-=======
-/* prueba
->>>>>>> parent of 9a2ede9... .
-=======
-/* prueba
->>>>>>> parent of 9a2ede9... .
+/* 
  * Developed by Makers Gandía in colaboration with Immersive Interactive Lab.
  * Authors: Alejandro Marco Ibáñez, María Balagué, Leonardo Rodríguez, Alejandro Castilla García, Jair López Gutiérrez
 */
