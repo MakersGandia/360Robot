70303f10ccf6b1d7159ec62ad13d6e4bdaccb11d
AlexCastillaG <alexcastilladj@gmail.com>
2018-07-09 10:20:52 +0200

    Revert "."
    This reverts commit 2b9437bb81552f78ad4f8dbdd9da58862820f66e.

--- 360Robot.ino
+++ 360Robot.ino
@@ -1,4 +1,12 @@
+<<<<<<< HEAD
 /* 
+=======
+<<<<<<< HEAD
+/* p
+=======
+/* prueba
+>>>>>>> parent of 9a2ede9... .
+>>>>>>> parent of 2b9437b... .
  * Developed by Makers Gandía in colaboration with Immersive Interactive Lab.
  * Authors: Alejandro Marco Ibáñez, María Balagué, Leonardo Rodríguez, Alejandro Castilla García, Jair López Gutiérrez
 */
